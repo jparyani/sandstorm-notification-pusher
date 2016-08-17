afe1a58d0f9305684c6be11bf85843707d05d281
Jason Paryani <github@jparyani.com>
2016-08-17 16:38:37 -0700

    Add priority and -t and -m options

--- main.js
+++ main.js
@@ -15,20 +15,25 @@ program
   .version('0.0.1')
   .arguments('<SANDSTORM_RESUME_TOKEN> <PUSHOVER_TOKEN> <PUSHOVER_USER_ID>')
   .option('-s, --server [value]', 'The server to connect to (defaults to oasis.sandstorm.io)', "wss://oasis.sandstorm.io")
+  .option('-m, --message-priority-regex [value]', 'A regex that when matched in a message will mark the message as high priority.')
+  .option('-t, --title-priority-regex [value]', 'A regex that when matched in a title will mark the message as high priority.')
   .action(function (resumeToken, pushoverToken, pushoverUserId) {
     TOKEN = pushoverToken;
     USER_ID = pushoverUserId;
-    startConnection(program.server, resumeToken);
+    m = program.messagePriorityRegex;
+    t = program.titlePriorityRegex;
+    startConnection(program.server, resumeToken, m && new RegExp(m), t && new RegExp(t));
   })
   .parse(process.argv);
 
-function sendMessage(title, message) {
+function sendMessage(title, message, priority) {
   request.post("https://api.pushover.net/1/messages.json", {
     form: {
       token: TOKEN,
       user: USER_ID,
       title: title,
       message: message,
+      priority: priority || 0,
     },
   });
 }
@@ -52,10 +57,10 @@ function sendError(err) {
     err: err,
   }));
 
-  sendMessage('Error in Sandstorm push server', err + "");
+  sendMessage('Error in Sandstorm push server', err + "", 1);
 }
 
-function startConnection(server, resumeToken) {
+function startConnection(server, resumeToken, messageRegex, titleRegex) {
   const Asteroid = asteroid.createClass();
   // Connect to a Meteor backend
   const connection = new Asteroid({
@@ -72,7 +77,14 @@ function startConnection(server, resumeToken) {
     if (res.collection === "desktopNotifications") {
       const msg = res.fields;
       const appActivity = msg.appActivity;
-      sendMessage(appActivity.user.name + " " + appActivity.actionText.defaultText, appActivity.body.defaultText);
+      let priority = 0;
+      if (messageRegex && appActivity.body.defaultText.match(messageRegex)) {
+        priority = 1;
+      }
+      if (titleRegex && appActivity.actionText.defaultText.match(titleRegex)) {
+        priority = 1;
+      }
+      sendMessage(appActivity.user.name + " " + appActivity.actionText.defaultText, appActivity.body.defaultText, priority);
     }
   });
 
